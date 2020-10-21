e70bff06d3d4d3b22be752072f6359795b9317e7
millegloerfelt-tarp <milletarp@live.se>
2020-10-21 12:45:21 +0200

    dasdl

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
+</html>
